0532499ae5ff22f1b85312ecb0558fa89def51b1
Priya Power <priyajag72@gmail.com>
2020-08-25 07:17:04 -0600

    Add welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "/", to: 'welcome#index'
+
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
   get '/shelters/:id', to: 'shelters#show'
---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe "Welcome Page", type: :feature do
+  describe "As a visitor" do
+    it "can have a welcome page" do
+      visit "/"
+      expect(page).to have_content("Welcome to Adopt Don't Shop")
+      expect(page).to have_content("A Denver Based Pet Adoption Website")
+    end
+
+    it "can see links for ALL PETS at top of every html page" do
+      visit "/"
+      expect(page).to have_link("All Pets")
+      click_link "All Pets"
+      expect(current_path).to eq("/pets")
+    end
+
+    it "can see links for ALL SHELTERS at top of every html page" do
+      visit "/"
+      save_and_open_page
+      expect(page).to have_link("All Shelters")
+      click_link "All Shelters"
+      expect(current_path).to eq("/shelters")
+    end
+
+    # it "can have a nav bar"
+  end
+end
