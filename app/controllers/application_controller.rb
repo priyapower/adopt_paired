3ee41cbd977a1c710605c2c0dcd5b793f982f182
Priya Power <priyajag72@gmail.com>
2020-08-31 18:27:48 -0600

    Add helper method applied_pets to AppController, returns array of pet ids you've applied for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
-  helper_method :favorites
-
+  helper_method :favorites, :applied_pets
   def favorites
     @favorites ||= Favorite.new(session[:favorite])
+  end
+
+  def applied_pets
+    PetApply.pluck(:pet_id)
   end
 end
